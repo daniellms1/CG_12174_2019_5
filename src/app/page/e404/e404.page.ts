732f28d4c66d3a0121d1c39113b0a7cd507f3a6c
daniellms1 <58314052+daniellms1@users.noreply.github.com>
2021-07-07 21:31:24 -0300

    tela login - logout

--- src/app/page/e404/e404.page.ts
+++ src/app/page/e404/e404.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-e404',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class E404Page implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth) { }
 
   ngOnInit() {
   }
